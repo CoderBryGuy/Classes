16106d16d781935d6fe6c8270d641223da5d0b94
bryan <bryan.dov.bergman@gmail.com>
2019-04-14 14:24:49 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        System.out.println("Model is " + porsche.getModel());
+
+        porsche.setModel("911");
+
+        System.out.println("Model is " + porsche.getModel());
+
+
+        porsche.setModel("porsche");
+
+        System.out.println("Model is " + porsche.getModel());
+
+
+    }
+}
